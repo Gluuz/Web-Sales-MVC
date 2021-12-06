925f4994a1b0fe42f12d53eecde85acceec36e5f
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-12-06 19:26:04 -0300

    First commit and Razor pages tests

--- WebSalesMVC/Controllers/HomeController.cs
+++ WebSalesMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebSalesMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Web Sales MVC App";
 
             return View();
         }
