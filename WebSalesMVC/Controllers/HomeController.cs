c29b8baf1e2a3a99af590fc233778ef1ec2d308e
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-12-06 19:58:24 -0300

    First Model-Controller-View - Department

--- WebSalesMVC/Controllers/HomeController.cs
+++ WebSalesMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebSalesMVC.Models;
+using WebSalesMVC.Models;
 
 namespace WebSalesMVC.Controllers
 {
---
+++ WebSalesMVC/Models/DepartmentModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace WebSalesMVC.Models
+{
+    public class DepartmentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
