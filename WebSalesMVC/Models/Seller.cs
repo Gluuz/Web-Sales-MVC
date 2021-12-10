3027a406ddbd94f9f6927db90b4ee847ce176323
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-12-10 09:37:27 -0300

    Validation

--- WebSalesMVC/Models/Seller.cs
+++ WebSalesMVC/Models/Seller.cs
@@ -9,20 +9,33 @@ namespace WebSalesMVC.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+
+        [Required(ErrorMessage ="{0} Required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size must be between {2} and {1}")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Enter a valid {0}")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 50000.0, ErrorMessage = "Enter a valid {0}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public DepartmentModel Deparment { get; set; }
+
         [Display(Name = "Department Id")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
