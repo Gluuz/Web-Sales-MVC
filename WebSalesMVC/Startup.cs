baaa687a17587d7888b13810f72d66fc97ba74a9
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-12-10 08:51:54 -0300

    App locale, number and date fomatting

--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebSalesMVC.Data;
 using WebSalesMVC.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace WebSalesMVC
 {
@@ -48,6 +50,15 @@ namespace WebSalesMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
